9c3ba06fa8bcf2d51ad8977ed9da3e70162a30aa
siddheshCS23155 <156663289+siddheshCS23155@users.noreply.github.com>
2024-02-05 17:25:21 +0530

    Add files via upload

---
+++ table.h
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1 align="center">TIME TABLE</h1>
+    <table align="center" border="100">
+       <tr>
+        <th rowspan="2">DAY</th>
+        <th>1</th>
+        <th>2</th>
+        <th>3</th>
+        <th style="width: 10cap;" style="background-color: firebrick;"  rowspan="8">R<br>E<br>C<br>E<br>S<br>S</th>
+        <th>5</th>
+        <th>6</th>
+        <th>7</th>
+       </tr>
+       <tr>
+     >
+        <td>10.30-11.30</td>
+        <td>11.30-12.30</td>
+        <td>12.30-01.30</td>
+        <td>2.30-3.30</td>
+        <td align="center">3.30-4.30</td>
+        <td>4.30-5.30</td>
+       </tr>
+       <tr>
+        <td>Monday</td>
+        <td align="center">EC(NZ) <br> (F201)</td>
+        <td align="center">ACP(YK) <br> (F201)</td>
+        <td align="center">Self Learning</td>
+        <td align="center">T.G Hour <br> (ND/HR) </td>
+        <td colspan="2" align="center">practical <br> WD(SC/MK/AT)(M-309)</td> 
+       </tr>
+       <tr>
+        <td>Tuseday</td>
+        <td align="center">ACP(YK) <br> (F201)</td>
+        <td colspan="2" align="center">practical <br> WD(SC/MK/AT)(M-309)</td>
+        <td align="center">EC(NZ) <br> (F201) </td>
+        <td colspan="2" align="center">LLC2 <br> (ND/HR)</td> 
+        
+
+       </tr>
+       <tr>
+        <td>Wednesday</td>
+        <td align="center">M-II(HR) <br> (F201)</td>
+        <td colspan="2" align="center">practical <br> (F201)</td>
+        <td align="center">EC(NZ)<br>(F201)</td>
+        <td align="center">FORAM <br> (ND/HR)</td> 
+        <td align="center">SPORTS</td> 
+       </tr>
+       <tr>
+        <td>Thusday</td>
+        <td align="center">M-II(HR) <br> (F201)</td>
+        <td align="center">TUTORIAL <br> (F201)</td>
+        <td align="center">EE (NS) <br>(F201)</td>
+        <td align="center">make up classes </td>
+        <td colspan="2" align="center">practical</td> 
+       </tr>
+       <tr>
+        <td>Friday</td>
+        <td align="center">EE(NS) <br> (F201)</td>
+        <td align="center">TUTORIAL <br> (F201)</td>
+        <td align="center">M-II (HR) <br>(F201)</td>
+        <td align="center">Labrary <br> (F203)</td>
+        <td colspan="2" align="center">practical</td> 
+       </tr>
+       <tr>
+        <td>Saturday</td>
+        <td align="center">CE(IK) <br> (F201)</td>
+        <td colspan="2" align="center">Practical <br> (F201)</td>
+        <td align="center">LLC-2 </td>
+        <td colspan="2" align="center">practical</td> 
+       </tr>
+</body>
+</html>
